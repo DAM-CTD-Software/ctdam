3181478652a39a57a1a63042e440fe6da76548ec
Emil Michels <emil.michels@iow.de>
2026-04-14 16:34:24 +0200

    feat: auto-load sensor mapping

--- src/ctdam/__init__.py
+++ src/ctdam/__init__.py
@@ -1,1 +1,20 @@
+import logging
+import tomllib
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
 APPNAME='ctdam'
+SENSOR_MAPPING_FILE = Path(__file__).parent.joinpath(
+    "conv",
+    "sensor_mapping.toml"
+)
+
+if not SENSOR_MAPPING_FILE.exists():
+    SENSOR_MAPPING_FILE.touch()
+    logger.info(
+        f"No sensor mapping file found in {SENSOR_MAPPING_FILE}. Created a blank one."
+    )
+
+with open(SENSOR_MAPPING_FILE, "rb") as file:
+    SENSOR_MAPPING = tomllib.load(file)
