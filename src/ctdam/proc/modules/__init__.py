e8dbccac1a634ee2f92b6b2a3ee5e71b286b4a7a
Clara <clara-TU@MacBook-Pro-109.local>
2026-07-09 10:01:31 +0200

    feat(proc): added file for cast_borders module

--- src/ctdam/proc/modules/__init__.py
+++ src/ctdam/proc/modules/__init__.py
@@ -1,4 +1,5 @@
 from .air_pressure_correction import *
 from .available_modules import *
+from .detect_cast_borders import *
 from .geomar_wildedit import *
 from .seabird_functions import *
---
+++ src/ctdam/proc/modules/detect_cast_borders.py
@@ -0,0 +1,50 @@
+from ctdam.conv.cast_borders import get_cast_borders
+from ctdam.proc.module import Module
+
+
+class CastBorders(Module):
+    """
+    Detect cast borders from pressure data.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "cast_borders"
+
+    def transformation(self) -> bool:
+        self.check_whether_working_on_binned_data()
+
+        arguments = self.arguments.copy()
+
+        pressure_parameter = arguments.pop("pressure_parameter", "prDM")
+        crop = arguments.pop("crop", False)
+
+        pressure = self.ds[pressure_parameter].data
+
+        borders = get_cast_borders(
+            pressure=pressure,
+            **arguments,
+        )
+
+        self.cast_borders = borders
+
+        self.arguments["pressure_parameter"] = pressure_parameter
+        self.arguments["crop"] = crop
+        self.arguments["down_start"] = borders["down_start"]
+        self.arguments["down_end"] = borders["down_end"]
+
+        if "up_start" in borders:
+            self.arguments["up_start"] = borders["up_start"]
+        if "up_end" in borders:
+            self.arguments["up_end"] = borders["up_end"]
+
+        if crop:
+            self._crop_to_downcast(
+                start=borders["down_start"],
+                end=borders["down_end"],
+            )
+
+        return True
+
+    def _crop_to_downcast(self, start: int, end: int) -> None:
+        self.ds = self.ds.isel(time=slice(start, end))
