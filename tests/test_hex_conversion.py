1d66f6c643132d5e043974c5636f6e2d677c0c7e
Emil Michels <emil.michels@iow.de>
2026-07-28 16:26:51 +0200

    feat: hex to xarray conversion

---
+++ tests/test_hex_conversion.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import pytest
+from conftest import hex_path
+from ctdam.parser.read_ctd_data import read_hex
+import xarray as xr
+
+
+@pytest.fixture(params=hex_path.glob("*.hex"), scope="class")
+def ds(request):
+    try:
+        return read_hex(request.param)
+    # TODO: check on hex to xmlcon mapping in HexFile
+    except AttributeError:
+        return
+    # TODO: implement time gap handling
+    except ValueError:
+        return
+
+
+class TestHexConversion:
+    def test_calling(self, ds):
+        pass
+
+    @pytest.mark.skip(reason="Not implemented")
+    def test_cnv_export(self, ds, create_files):
+        if not isinstance(ds, xr.Dataset):
+            pytest.skip()
+        file_path = (
+            hex_path / f"out_test_{Path(ds.attrs['path_to_source_file']).name}"
+        )
+        ds.export.to_cnv(file_path)
+        if not create_files:
+            file_path.unlink()
