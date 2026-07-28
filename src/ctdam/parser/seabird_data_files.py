42431c41035c9562b6915b81786ba7d3bbf3d0fb
Emil Michels <emil.michels@iow.de>
2026-07-28 16:24:46 +0200

    feat: restructured HexFile and XMLCONFile with new conversion methods

--- src/ctdam/parser/seabird_data_files.py
+++ src/ctdam/parser/seabird_data_files.py
@@ -1,12 +1,17 @@
 import logging
+import re
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Tuple
 
 import numpy as np
+import odf.sbe.accessors
+import xarray as xr
 import xmltodict
+from odf.sbe.io import read_hex
 
 from ctdam.exceptions import UnexpectedFileFormat
+from ctdam.parser.xmlfiles import XMLCONFile
 from ctdam.utils import (
     create_event_string,
     extract_sensor_name,
@@ -358,3 +363,168 @@ class CnvFile(SeabirdDataFile):
         return np.array(
             [(self.start_time + d).timestamp() for d in data]
         ).astype("float")
+
+
+class HexFile(SeabirdDataFile):
+    """
+    A representation of a .hex file as used by SeaBird.
+
+    When no corresponding .xmlcon file given, a search algorithm is used
+    to determine the matching .xmlcon automatically.
+
+    Parameters
+    ----------
+    path_to_file: Path | str:
+        The path to the file
+    path_to_xmlcon: Path | str
+        An optional path to the corresponding .xmlcon file
+    """
+
+    def __init__(
+        self,
+        path_to_file: Path | str,
+        path_to_xmlcon: Path | str = "",
+        *args,
+        **kwargs,
+    ):
+        # force loading only metadata
+        super().__init__(path_to_file, True)
+        self.xmlcon = self.get_corresponding_xmlcon(path_to_xmlcon)
+        self.raw_ds = self.parse_hex(path_to_file)
+        self.unixtime = self._handle_time()
+
+    def parse_hex(self, hex: Path | str) -> xr.Dataset:
+        raw_ds = read_hex(hex)
+        serialized_ds = raw_ds.sbe.serialize()
+        return serialized_ds
+
+    def _get_time_gaps(self) -> dict:
+        """
+        Detect missing data points in the raw CTD data.
+
+        Parameters
+        ----------
+        raw_data: pd.DataFrame
+            Pandas DataFrame holding the structured CTD raw data
+
+        Returns
+        -------
+        A dictionary with the indices and sizes of data gaps.
+        """
+        data_integrity = self.raw_ds["mod"].values.astype(int)
+        diff = np.diff(data_integrity) % 256
+        gaps = np.where(diff != 1)[0]
+        gap_sizes = {a: diff[a] - 1 for a in gaps}
+        return gap_sizes
+
+    def _handle_time(self):
+        """
+        Fills data gaps and creates correct time arrays.
+
+        Data gaps are filled with NaNs.
+        Time array created are
+            1) a time elapsed one, counting the seconds from the cast start,
+            2) a unix timestamp.
+
+        Parameters
+        ----------
+        data_size: int
+            The size of the data, to enable skipping of upcast time gaps
+        """
+        gap_sizes = self._get_time_gaps()
+        # TODO:
+        # implement the automatic filling of time gaps with nans in all
+        # parameter arrrays
+        # for index, gap_size in sorted(gap_sizes.items(), reverse=True):
+        #     if index > data_size:
+        #         continue
+        #     for param in parameters.values():
+        #         param.data = np.insert(
+        #             param.data, index, [np.nan] * (gap_size)
+        #         )
+
+        seconds_since_start = np.cumsum(
+            np.concatenate(
+                [
+                    [0.0],
+                    np.ones(
+                        (self.raw_ds.scan.size + sum(gap_sizes.values())) - 1
+                    )
+                    * (1 / 24),
+                ]
+            )
+        )
+
+        start_time_posix = self.start_time.timestamp()
+        corrected_time_array = seconds_since_start + start_time_posix
+
+        return corrected_time_array.astype("float")
+
+    def get_corresponding_xmlcon(
+        self,
+        path_to_xmlcon: Path | str = "",
+    ) -> XMLCONFile | None:
+        """
+        Finds the best matching .xmlcon file inside the same directory.
+
+        The logics works as follows:
+
+        - if an .xmlcon of the same name exists, take that
+        - else, find all .xmlcons of the same cruise inside the given
+          directory and use the one used by the previous .hex file, sorted
+          by file name.
+
+        Parameters
+        ----------
+        path_to_xmlcon: Path | str:
+            A path to an .xmlcon file, if not existent, use the search algorithm
+        """
+        # xmlcon path given, test and use it
+        if isinstance(path_to_xmlcon, str):
+            if path_to_xmlcon:
+                return XMLCONFile(path_to_xmlcon)
+        else:
+            if path_to_xmlcon.exists():
+                return XMLCONFile(path_to_xmlcon)
+        # no xmlcon path, lets find one in the same dir
+        # get all xmlcons in the dir
+        # first, try the very same name
+        same_name_xmlcon = self.path_to_file.with_suffix(".XMLCON")
+        if same_name_xmlcon.exists():
+            return XMLCONFile(same_name_xmlcon)
+
+        # otherwise, take the xmlcon of the previous hex file, sorted by name
+        # use either the extracted cruise name or the first five letters for
+        # searching for xmlcons of the same cruise
+        if self.cruise:
+            prefix = (
+                re.split(r"[_\-\/]", self.cruise.lower())[0]
+                if any(x in self.cruise.lower() for x in ["_", "-", "/"])
+                else self.cruise.lower()
+            )
+        else:
+            prefix = self.file_name.lower()[:5]
+        xmlcons = [
+            xmlcon
+            for xmlcon in sorted(
+                self.file_dir.glob("*.XMLCON", case_sensitive=False)
+            )
+            if xmlcon.stem.lower().startswith(prefix)
+        ]
+        if not xmlcons:
+            return None
+
+        # looking back and using xmlcon from the previous hex file
+        all_hexes = [
+            hex
+            for hex in sorted(
+                self.file_dir.glob("*.hex", case_sensitive=False)
+            )
+            if hex.stem.lower().startswith(prefix)
+        ]
+        index = all_hexes.index(self.path_to_file)
+        previous_hexes = all_hexes[:index]
+        if previous_hexes:
+            return HexFile(previous_hexes[-1]).xmlcon
+
+        return XMLCONFile(xmlcons[0])
