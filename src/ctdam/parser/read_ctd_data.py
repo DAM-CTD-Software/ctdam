04098a4cfa4893d4f08d34a943b6a8c6a91068a8
Emil Michels <emil.michels@iow.de>
2026-07-28 16:24:46 +0200

    feat: restructured HexFile and XMLCONFile with new conversion methods

--- src/ctdam/parser/read_ctd_data.py
+++ src/ctdam/parser/read_ctd_data.py
@@ -1,17 +1,19 @@
 import logging
 from pathlib import Path
+from inspect import getmembers, isfunction
 
 import numpy as np
 import xarray as xr
 
 import ctdam.parser.custom_xarray_accessors
 from ctdam import PARAMETER_MAPPING, SBS_NAME_MAPPING
+from ctdam.conv import raw_conversion
 from ctdam.conv.unit_conversion import (
     get_potential_density,
     oxygen_mlperl_to_umolperkg,
     oxygen_umolperl_to_umolperkg,
 )
-from ctdam.parser.seabird_data_files import CnvFile
+from ctdam.parser.seabird_data_files import CnvFile, SeabirdDataFile, HexFile
 
 logger = logging.getLogger(__name__)
 
@@ -186,11 +188,46 @@ def read_cnv(
     return ds
 
 
+def read_hex(
+    path_to_hex_file: Path | str,
+) -> xr.Dataset:
+    hex_file = HexFile(path_to_hex_file)
+
+    coords = create_array_coords(hex_file)
+    attrs = create_array_attrs(hex_file)
+    # raw data version as fall back
     ds = xr.Dataset(
-        cf_xarray_data,
+        hex_file.raw_ds.data_vars,
         coords=coords,
         attrs=attrs,
     )
+
+    if hex_file.xmlcon:
+        df = hex_file.xmlcon.coefficients
+        sensor_data = [
+            hex_file.raw_ds[v].data.astype(float)
+            for v in hex_file.raw_ds.data_vars
+            if v.startswith(("f", "v"))
+        ]
+        if len(df.columns) == len(sensor_data):
+            # drop placeholder raw data
+            ds = ds.drop_vars(lambda x: x.data_vars)
+            for sensor, raw_data in zip(df.columns, sensor_data):
+                name = (
+                    sensor.replace("_Sensor", "").replace("Sensor", "").lower()
+                )
+                name = name[:-1] if name[-1] in ["1", "2"] else name
+                logger.error(name)
+                if not name in PARAMETER_MAPPING:
+                    continue
+                conv_functions = {
+                    n: f for n, f in getmembers(raw_conversion, isfunction)
+                }
+                if not name in conv_functions:
+                    continue
+                converted_data = conv_functions[name](raw_data, df[sensor])
+                logger.error(converted_data)
+                ds.add.parameter(name, converted_data)
 
     return ds
 
@@ -201,9 +238,9 @@ def read_ctd_data(path_to_ctd_data_file: Path | str) -> xr.Dataset:
 
     if suffix == "cnv":
         return read_cnv(path_to_ctd_data_file)
+    elif suffix == "hex":
+        return read_hex(path_to_ctd_data_file)
     # TODO: implement these
-    # elif suffix == "hex":
-    #     pass
     # elif suffix == "tob":
     #     pass
     # elif suffix == "ctd":
