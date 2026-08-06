6096cf8f3a1dd19f608284e2f009964e2ecc7104
Emil Michels <emil.michels@iow.de>
2026-08-06 08:41:32 +0200

    test: hex conversion comparison with datcnv result

--- tests/test_hex_conversion.py
+++ tests/test_hex_conversion.py
@@ -2,19 +2,46 @@ from pathlib import Path
 
 import pytest
 import xarray as xr
-from conftest import hex_path
+from conftest import cnv_path, hex_path
+from numpy.testing import assert_allclose
 
-from ctdam.parser.read_ctd_data import read_hex
+from ctdam.parser.read_ctd_data import read_cnv, read_hex
 
 
 @pytest.fixture(params=hex_path.glob("*.hex"), scope="class")
 def ds(request):
+    if request.param.stem == "EMB379_000-00_SF_0001":
+        pytest.skip("PyroScience Oxygen Sensor not supported yet.")
     return read_hex(request.param)
 
 
 class TestHexConversion:
-    def test_calling(self, ds):
-        pass
+    def test_datcnv_comparison(self, ds):
+        assert "conductivity" in ds.data_vars
+        file_name = Path(ds.attrs["path_to_source_file"]).name
+        try:
+            comp_ds = read_cnv((cnv_path / file_name).with_suffix(".cnv"))
+        except FileNotFoundError:
+            pytest.skip(f"No comparison file for {file_name}")
+        comparison_file_length = comp_ds.access.size()
+        for parameter in ds.data_vars:
+            if "qc" in parameter:
+                continue
+            try:
+                comparison = comp_ds[parameter].data
+            except KeyError:
+                continue
+            if parameter in [
+                # "oxygen",
+                "par_biosphericallicorchelsea",
+            ]:
+                continue
+            assert_allclose(
+                ds[parameter].data[:comparison_file_length],
+                comparison,
+                # rtol=0.002,
+                rtol=0.01,
+            )
 
     @pytest.mark.skip(reason="Not implemented")
     def test_cnv_export(self, ds, create_files):
