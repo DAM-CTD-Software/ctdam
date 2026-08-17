243de882867f2f94a5c6b5746464fa6e50194804
Emil Michels <emil.michels@iow.de>
2026-08-17 15:20:03 +0200

    test: legacy test clean up

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -23,3 +23,17 @@ def test_bottle_reading():
     btl = BottleFile(test_btl)
     btl.selecting_rows()
     assert not "Statistic" in btl.df.columns
+
+
+@pytest.mark.parametrize(
+    ("input_name", "expected_output"),
+    [
+        ("EMB346_082-01", "EMB346_082-01"),
+        ("msm123-011_02", "MSM123_011-02"),
+        ("M42-52-1", "M42_052-01"),
+        ("So308-2_008_3", "SO308_2_008-03"),
+    ],
+)
+def test_event_name_parsing(input_name, expected_output):
+    cruise, station = read_event_name(input_name)
+    assert create_event_string(cruise, station) == expected_output
