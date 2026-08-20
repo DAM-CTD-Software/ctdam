1bcee32e94338299493049bbfe044b98da8eb502
Emil Michels <emil.michels@iow.de>
2026-08-20 11:26:36 +0200

    refactor: collect high level entry functions in one location

--- src/ctdam/proc/__init__.py
+++ src/ctdam/proc/__init__.py
@@ -1,9 +1,7 @@
-from .entry import process
 from .utils import fill_file_type_dir, is_directly_measured_value
 from .workflow import Workflow
 
 __all__ = [
-    "process",
     "is_directly_measured_value",
     "fill_file_type_dir",
     "Workflow",
