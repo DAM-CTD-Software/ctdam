2731565f4120b5365672a0a24b09a02e01f1bcb9
Ali N <ali.najjar.mw4@gmail.com>
2026-09-11 10:54:16 +0200

    refactor(conv): simplify user polynomial sensor conversion

--- tests/test_raw_conversion.py
+++ tests/test_raw_conversion.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_allclose
 from ctdam.conv import raw_conversion
 
 
-def test_flow_meter_conversion():
+def test_user_polynomial_conversion():
     voltage = np.array([0.0, 1.0, 2.0])
     cfgp = pd.Series(
         {
@@ -20,19 +20,6 @@ def test_flow_meter_conversion():
     )
 
     expected = np.array([1.0, 10.0, 49.0])
-    actual = raw_conversion.flow_meter(voltage, cfgp)
+    actual = raw_conversion.userpolynomial(voltage, cfgp)
 
     assert_allclose(actual, expected)
-
-
-def test_pyro_oxygen_conversion():
-    voltage = np.array([0.0, 1.0, 2.0, np.nan, 1.0])
-    potential_density = np.array([25.0, 25.0, 30.0, 25.0, np.nan])
-    calibration = Munch(A0="0.0", A1="200.0", A2="0.0", A3="0.0")
-    cfgp = pd.Series({"cal": calibration})
-    expected = np.array([0.0, 195.12, 388.35, np.nan, np.nan])
-
-    # For example, 200 µmol/L at 1.025 kg/L gives 195.12 µmol/kg.
-    actual = raw_conversion.pyro_oxygen(voltage, cfgp, potential_density)
-    # Allow for rounding the expected values to two decimal places.
-    assert_allclose(actual, expected, rtol=0, atol=0.005)
